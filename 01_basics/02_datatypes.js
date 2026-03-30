66f0b374c8b4cbf4bb2092ee6151284a82661dbb
Chandrakant Deshmukh <chandrakant.chat360@gmail.com>
2026-03-30 11:44:45 +0000

    datatypes add

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -7,3 +7,23 @@
 console.log(3 + 3)
 
 console.log("chandrakant")
+
+
+let name = "chandrakant"
+let age = 29
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value (empty value)
+// undefined => value is not define this is the undefined value
+// symbol =>  unique 
+
+
+// object  
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
